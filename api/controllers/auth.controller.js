78b61057f655e18ab0b0a5b1b3d5d77a7edb8b30
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-11-02 22:00:23 +0400

    create a middleware and function to handle a possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     //show request body from postman in terminal - fe: username: 'test', email: 'test@gmail.com', password: 'vasil'
     //console.log(req.body);
     const {username, email, password} = req.body;
@@ -13,7 +13,9 @@ export const signup = async (req, res) => {
         res.status(201).json('User created successfully');
     }
     catch(error){
-        res.status(500).json(error.message);
+        //res.status(500).json(error.message);
+        //error handling comes from our middleware from index.js
+        next(error);
     }
     
 }
