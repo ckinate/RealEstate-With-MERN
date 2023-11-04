5987d7e75f4f58f7d12916368a71f3547681a07c
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-11-04 17:46:43 +0400

    complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,6 +27,7 @@ export const signin = async (req, res, next) => {
         //search user inside mongo atlas db
         const validUser = await User.findOne({email});
         if(!validUser) return next(errorHandler(404, 'User not found'));
+        //compare passwords in db and password textbox
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(404, 'Not valid password'));
         //if both username and password is valid, then we need:
